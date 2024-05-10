e2df41e8866549e68d1df3573d76235cdf94da00
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-09 23:59:33 -0500

    feat: :sparkles: add cryptoDisplay section

--- src/components/CryptoDisplay.tsx
+++ src/components/CryptoDisplay.tsx
@@ -1,7 +1,22 @@
 export default function CryptoDisplay() {
   return (
-    <div>
-      display
+    <div className="space-y-6">
+      <h2 className="text-center font-semibold text-3xl">
+        Cotización
+      </h2>
+      <div className="flex justify-center items-center gap-4">
+        <div className="basis-auto">
+          <img src="/bg.jpg" alt="cryptocoin" className="" />
+        </div>
+        <div className="basis-auto">
+          <p className="">
+            El precio es de {""}
+            <span className="font-semibold">
+              $62,919.8
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/CryptoForm.tsx
+++ src/components/CryptoForm.tsx
@@ -1,7 +1,39 @@
 export default function CryptoForm() {
   return (
-    <div>
-      form
-    </div>
+    <form className="space-y-5">
+      <div className="space-y-3">
+        <label htmlFor="commoncoin"
+          className="block text-lg"
+        >
+          Moneda:
+        </label>
+        <select name="commoncoin" id="commoncoin"
+          className="w-full px-2 py-2 rounded-md text-lg"
+        >
+          <option value="" disabled>--Seleccione--</option>
+          <option value="DOL">Dolar USA</option>
+          <option value="PES">Peso Mexicano</option>
+        </select>
+      </div>
+
+      <div className="space-y-3">
+        <label htmlFor="cryptocoin"
+          className="block text-lg"
+        >
+          Criptomoneda:
+        </label>
+        <select name="cryptocoin" id="cryptocoin"
+          className="w-full px-2 py-2 rounded-md text-lg"
+        >
+          <option value="" disabled>--Seleccione--</option>
+          <option value="BTC">Bitcoin</option>
+          <option value="ETH">Ethereum</option>
+        </select>
+      </div>
+
+      <input type="submit" value="Cotizar"
+        className="w-full bg-emerald-500 hover:bg-emerald-600 uppercase text-sm font-semibold py-2 cursor-pointer rounded-md text-white"
+      />
+    </form >
   )
 }
